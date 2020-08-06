cfdf94f3899e1af504977a9b4bc495d29ecb22f1
Duncan Sutter <dsutter@qualivera.com>
2020-08-06 11:35:41 -0400

    Fix copy-sites.sh so it works

--- php-pre-start/copy-sites.sh
+++ php-pre-start/copy-sites.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ -f /opt/app-root/src/html/sites/default/settings.php ]
-then
+if [ -f /opt/app-root/src/html/sites/default/settings.php ]; then
     echo "Data exists, skip copying default data."
 else
     echo "Data dir empty.  Copy default data."
-    if [ -d /opt/app-root/src/data ]
-    then
-        if [ ! -d /opt/app-root/src/data/sites ]
-        then
-            mkdir /opt/app-root/src/data/sites
-        fi
-    else
-        cp -R /opt/app-root/src/html/tempsite/* /opt/app-root/src/html/sites
+    if [ -d /opt/app-root/src/data && ! -d /opt/app-root/src/data/sites ]; then
+        mkdir /opt/app-root/src/data/sites
     fi
+    cp -R /opt/app-root/src/html/tempsite/* /opt/app-root/src/html/sites
 fi
