1668121234b475011e8ae451fa68f6118a4d9544
Duncan Sutter <dsutter@qualivera.com>
2021-01-21 16:49:22 -0500

    cleanup

--- .gitlab-ci/env.sh
+++
@@ -1,3 +0,0 @@
-# Docker image tag (if tag, use tag else generate a unique identifier)
-export VERSION=$(git describe --tags 2>/dev/null || echo 0.0.0)
-export DOCKER_TAG=$(echo $VERSION | sed 's/8\.x-//g')-$CI_PROJECT_ID.$CI_PIPELINE_IID
