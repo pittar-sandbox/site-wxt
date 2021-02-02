5837166f6f83da5d51975272491daa3242cdd8e0
Duncan Sutter <dsutter@qualivera.com>
2021-02-02 14:23:46 -0500

    Test php startup script

---
+++ php-pre-start/startup.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+file_put_contents('/opt/app-root/src/a', "This is a file.\n");
+
+file_put_contents('b', "This is the b file.\n");
+
+include_once 'html/sites/sites.php';
+
+file_put_contents('/opt/app-root/src/sites', print_r($sites, true));
+
+chdir('html');
+foreach ($sites as $name) {
+  link('.', $name);
+}
