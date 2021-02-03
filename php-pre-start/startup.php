a134669fdb48ea9cbfa66f275e0aa96a48226eff
Duncan Sutter <dsutter@qualivera.com>
2021-02-03 13:03:03 -0500

    Fix vendor for bitbucket repo.

--- php-pre-start/startup.php
+++ php-pre-start/startup.php
@@ -1,13 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-file_put_contents('/opt/app-root/src/a', "This is a file.\n");
-
-file_put_contents('b', "This is the b file.\n");
-
 include_once 'html/sites/sites.php';
-
-file_put_contents('/opt/app-root/src/sites', print_r($sites, true));
 
 chdir('/opt/app-root/src/html');
 foreach ($sites as $name) {
