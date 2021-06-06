0488915376d4bd2e21a77853c821b7f291207627
Andrew Pitt <apitt@redhat.com>
2021-06-06 16:58:40 -0400

    Update copy-sites.sh

--- php-pre-start/copy-sites.sh
+++ php-pre-start/copy-sites.sh
@@ -6,7 +6,6 @@ else
     echo "Data dir empty.  Copy default data."
     if [[ -d /opt/app-root/src/data && ! -d /opt/app-root/src/data/sites ]]; then
         mkdir /opt/app-root/src/data/sites
-        ln -s /opt/app-root/src/data/sites /opt/app-root/src/html/sites
     fi
     cp -R /opt/app-root/src/html/tempsite/* /opt/app-root/src/html/sites
 fi
